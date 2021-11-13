0949498e77ca9332c6e8bb326610593ae859cbfd
村上恭哉 <mkkyyyy@mkkyyyy.local>
2021-11-13 21:13:27 +0900

    rename

--- network.tf
+++ network.tf
@@ -8,28 +8,28 @@ resource "aws_vpc" "jphacks" {
     }
 }
 
-resource "aws_subnet" "public-1a" {
+resource "aws_subnet" "public_1a" {
     vpc_id                  = aws_vpc.jphacks.id
     cidr_block              = "10.0.0.0/24"
     map_public_ip_on_launch = true
     availability_zone       = "ap-northeast-1a"
 }
 
-resource "aws_internet_gateway" "public-1a-igw" {
+resource "aws_internet_gateway" "public_1a_igw" {
     vpc_id = aws_vpc.jphacks.id
 }
 
-resource "aws_route_table" "public-1a" {
+resource "aws_route_table" "public_1a" {
     vpc_id = aws_vpc.jphacks.id
 }
 
-resource "aws_route" "public-1a" {
-    route_table_id = aws_route_table.public-1a.id
-    gateway_id = aws_internet_gateway.public-1a-igw.id
+resource "aws_route" "public_1a" {
+    route_table_id = aws_route_table.public_1a.id
+    gateway_id = aws_internet_gateway.public_1a_igw.id
     destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "public-1a" {
-    subnet_id = aws_subnet.public-1a.id
-    route_table_id = aws_route_table.public-1a.id
+resource "aws_route_table_association" "public_1a" {
+    subnet_id = aws_subnet.public_1a.id
+    route_table_id = aws_route_table.public_1a.id
 }
